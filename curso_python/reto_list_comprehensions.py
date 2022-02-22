a4658dfc965ec2c6e059dc73c4dfba07488197cd
danielvinzia <danielvinzia@gmail.com>
2022-02-22 11:59:50 -0300

    clase de comprencion de lista y reto en python, concluido

---
+++ curso_python/reto_list_comprehensions.py
@@ -0,0 +1,13 @@
+def run():
+    """reto = [i for i in range(1, 10000) if i % 36 == 0]
+    
+    print(reto)"""
+    reto = [i for i in range(1, 10000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0]
+
+    print(reto)
+    
+    
+
+
+if __name__ == '__main__':
+    run()
