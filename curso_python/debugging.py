35b707c74519789eebbd9e83cf6751ba320391e3
danielvinzia <danielvinzia@gmail.com>
2022-03-02 09:43:47 -0300

    clase de debugging concluida

---
+++ curso_python/debugging.py
@@ -0,0 +1,16 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def run():
+    num = int(input("Enter a number: "))
+    print(divisors(num))
+    print("Finish the Daniel's program")
+
+
+if __name__ == '__main__':
+    run()
+    
